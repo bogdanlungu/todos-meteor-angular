95c82e73e303ccd58287b239d9284519e2c24875
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-02-23 22:59:10 +0200

    Test: add a meteor collection to the $scope object

--- todos-meteor-angular.js
+++ todos-meteor-angular.js
@@ -1,11 +1,29 @@
+Tasks = new Mongo.Collection('tasks');
+
+if (Tasks.find().count() === 0) {
+   Tasks.insert({
+    title: 'Buy grocerries',
+    text: 'From the marketplace'
+   });
+   Tasks.insert({
+     title: 'Fix the car',
+     text: 'At the service'
+    });
+   Tasks.insert({
+     title: 'Lear JavaScript',
+     text: 'From the books'
+   });
+}
+
 if (Meteor.isClient) {
   angular.module('simple-todos',['angular-meteor']);
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope',
     function ($scope) {
-      $scope.tasks = [
-        { text: 'This is task 1' },
-        { text: 'This is task 2' },
-        { text: 'This is task 3' }
-      ];
-  }]);
+      // $scope.tasks = $scope.$meteorCollection(Tasks); // deprecated
+      $scope.helpers({
+         tasks() {
+         return Tasks.find({}, {sort: {createdAt: -1}});
+         }
+      });
+    }]);
 }
